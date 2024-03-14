d54808da83b59a3e195aeb84f22d5118ded6a255
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-14 17:34:54 +0100

    feat: add the function to count the word from the right to the left inside a for cycle

--- js-palindroma/script.js
+++ js-palindroma/script.js
@@ -4,6 +4,18 @@
 // Su internet ci sono soluzioni con join e reverse, non potete usarle!
 
 
+const theResultOfPalindrome = isTheWordAPalindrom (userWord)
+
+// FUNCTIONS
+function isTheWordAPalindrom (word) {
+    let reverseWord = '';
+    for (let i = word.length - 1; i >= 0; i-- ) {
+        let letterWord = word[i];
+        console.log(letterWord);
+    }
+    
+}
+
 // Chiediamo all'utente di inserire una parola in un prompt
 // Scorriamo la parola da sinistra verso destra 
 // Scriviamola le lettere prese e concateniamole 
