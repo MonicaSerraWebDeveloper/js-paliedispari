7eb10b4af502327ac017f781ec5ecdacda3a9981
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-14 16:52:12 +0100

    start init palindroma

---
+++ js-palindroma/script.js
@@ -0,0 +1,11 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// Su internet ci sono soluzioni con join e reverse, non potete usarle!
+
+
+// Chiediamo all'utente di inserire una parola in un prompt
+// Scorriamo la parola da sinistra verso destra 
+// Scriviamola le lettere prese e concateniamole 
+// Se il risultato sarà uguale alla parola scritta dall'utente
+// Allora la parola è palindroma
