0ae4ab98355e694c8873740d676387234d01f371
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-14 13:55:21 +0100

    start init

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto
+
+// L'utente sceglie "pari" o "dispari"
+// Chiediamo all'utente un numero da 1 a 5 
+// Generiamo un numero random da 1 a 5 per il computer
+// Facciamo la somma del numero dell'utente con il numero del computer
+// Se il risultato della somma è pari restituiamo "pari"
+// Se il risultato della somma è dispari restituiamo "dispari"
+// Se il risultato tra pari e dispari corrisponde alla scelta dell'utente ha vinto l'utente
+// Se il risultato è diverso rispetto alla scelta dell'utente allora l'utente ha perso
+
